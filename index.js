d30242dfcdbc2408cf627526079f0816f5a658ea
Youssif94 <86874043+Youssif94@users.noreply.github.com>
2022-04-07 14:23:01 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// btns
+
+const btn = document.querySelectorAll('.btn')
+const submitB = document.querySelector('.submitB')
+const contentStart = document.querySelectorAll('.page1')
+const contentEnd = document.querySelectorAll('.page2')
+const rating = document.querySelector('.rating')
+
+// rating value
+let ratingStar;
+
+// looping through the buttons
+btn.forEach(btn=> btn.addEventListener('click',function(){
+    // looping through the buttons again to remove the 'pressed' class from the rest and add it to the clicked one
+    const btns = document.querySelectorAll('.btn')
+    btns.forEach(btn => btn.classList.remove('btnPressed'))
+    this.classList.toggle('btnPressed')
+    ratingStar = this.textContent
+    return rating
+}))
+
+submitB.addEventListener('click',function(){
+        // hide all the parts of the 1st section
+        contentStart.forEach(section => section.style.display ="none");
+        // show the parts of the 2nd section
+        contentEnd.forEach(section => section.classList.remove('page2'))
+        rating.textContent = `You have selected ${ratingStar} out of 5`
+})
